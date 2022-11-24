7c7d195e838f72e227334f0b651d5ea0fabffc33
carroco <carrodriguezco@gmail.com>
2022-11-23 20:11:22 -0500

    Inicio del proyecto

--- archivos/pruebas.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-dataframe = pd.read_csv("")
-columnas_valores = ["Valor del Contrato", "Valor de pago adelantado", "Valor Pendiente de pago", "Valor Pagado"]
-valores =  dataframe[columnas_valores].copy()
-print(valores)
